ea85efbc0ad49adfae9347f813411b88ad8210a0
Keith Floyd <keithfloyd2001@gmail.com>
2022-10-20 11:38:40 -0400

    Change fieldset to form

--- script.js
+++ script.js
@@ -7,12 +7,3 @@ let passwordConfirm = document.getElementById("user_passwordconfirmation");
 let submitButton = document.querySelector("#submitbutton");
 
 
-submitButton.addEventListener('click', test);
-
-function test() {
-    alert("First Name: " + firstName.value + "");
-    alert("Last Name: " + lastName.value + "");
-    alert("Email: " + email.value + "");
-    alert("Password: " + password.value + "");
-    alert("Password Confirm: " + passwordConfirm.value + "");
-}
