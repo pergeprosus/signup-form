0fb36fd8efd1d5841ace22132941bb7eef4f7414
Keith Floyd <keithfloyd2001@gmail.com>
2022-10-20 11:01:19 -0400

    Add all input elements to js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+let firstName = document.getElementById("first_name");
+let lastName = document.getElementById("last_name");
+let email = document.getElementById("user_email");
+let phoneNumber = document.getElementById("user_phonenumber");
 let password = document.getElementById("user_password");
-let passwordConfirm = document.querySelector("user_password");
+let passwordConfirm = document.getElementById("user_passwordconfirmation");
 let submitButton = document.querySelector("#submitbutton");
 
 
